7eb46dbc0545627613da47716b68347a11d6cd89
Haoran Ju <32230526+extremejhr@users.noreply.github.com>
2018-06-28 14:44:34 +0800

    Add files via upload
    Basic Framework in Class_Library.py, any changes will be updated in a new py file.

--- Class_Library.py
+++ Class_Library.py
@@ -16,7 +16,7 @@ Created on Wed Jun 27 12:29:34 2018
 # Import Modules
 #
 ###############################################################################
-from PIL import Image 
+from PIL import Image, ImageGrab 
 # pytesseract import module should be modified if 'Image has no attribute Image' appears.
 
 import pytesseract
@@ -29,7 +29,48 @@ pytesseract.pytesseract.tesseract_cmd = 'E:\\Development\\Tesseract-OCR\\tessera
 import pyautogui
 import win32gui # OCR combined with win32 get handler and position.
 
+import time
+
 ###############################################################################
+
+###############################################################################
+#
+# Window_Capture -> Image Capture (28.06.2018)
+#
+###############################################################################    
+    
+class Window_Capture(object):
+    
+    def __init__(self,title):
+        
+        self.title = title
+        
+        self.handle = None
+        
+    def Handle_Catch(self,hwnd,lParam):
+        
+        if win32gui.IsWindowVisible(hwnd):
+            
+            if self.title in win32gui.GetWindowText(hwnd):
+                
+                self.handle = hwnd     
+   
+    def Image_Capture(self):
+        
+        win32gui.EnumWindows(self.Handle_Catch, None)
+        
+        if self.handle is None:
+            
+            print('No Window Found!')
+            
+        else:
+            
+            bbox = win32gui.GetWindowRect(self.handle)
+            self.img = np.array(ImageGrab.grab(bbox))
+        
+        return self.img, self.handle
+    
+###############################################################################  
 
 ###############################################################################
 #
@@ -43,7 +84,7 @@ class Image_Segmentation(object):
         
         self.Operation_Image = Operation_Image
         
-    def Get_Region(self, plot_flag = None, Morph_kernel = None):
+    def Get_Region(self, plot_flag = None, Morph_kernel = None, Binary_Threshold = None):
         
         if plot_flag is None:
             
@@ -51,9 +92,15 @@ class Image_Segmentation(object):
             
         if Morph_kernel is None:
             
-            Morph_kernel = (5,5)     
+            Morph_kernel = (4,2)  
+            
+        if Binary_Threshold is None:
+            
+            Binary_Threshold = 190               
                    
-        self.img_initial = cv2.imread(self.Operation_Image)
+        #self.img_initial = cv2.imread(self.Operation_Image)
+        
+        self.img_initial = self.Operation_Image
         
         h, w, _ = self.img_initial.shape
         
@@ -61,15 +108,17 @@ class Image_Segmentation(object):
         
         # Delete the Long Edge
         
-        (_, thresh) = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY_INV)
+        (_, thresh) = cv2.threshold(gray, Binary_Threshold, 255, cv2.THRESH_BINARY_INV)     
         
         edges = cv2.Canny(thresh,100,200)
         
         lines = cv2.HoughLinesP(edges,1,np.pi/180,10, minLineLength = 20, maxLineGap = 0)
         
-        for i in range(len(lines)):
-            line = lines[i]    
-            cv2.line(thresh,(line[0][0],line[0][1]), (line[0][2],line[0][3]),(0,0,0),5)
+        if lines is not None:
+
+            for i in range(len(lines)):
+                line = lines[i]    
+                cv2.line(thresh,(line[0][0],line[0][1]), (line[0][2],line[0][3]),(0,0,0),5)
             
         # Segmentation
         # -> Can be optimized further in order to find more accurate parameter of morphology.
@@ -81,10 +130,10 @@ class Image_Segmentation(object):
         
         closed = cv2.morphologyEx(thresh_copy, cv2.MORPH_CLOSE, kernel_morph) 
         
-        kernel_dilate = np.uint8(np.ones((6,7)))
-        kernel_dilate[3,:]=0
+        kernel_dilate = np.uint8(np.ones((3,5)))
+        kernel_dilate[1,:]=0
         
-        closed = cv2.dilate(closed, kernel_dilate, 5)
+        closed = cv2.dilate(closed, kernel_dilate, 1)
         
         #blurred = cv2.GaussianBlur(closed, (9, 9),0) # Gaussian Blur reduce noise
         blurred = closed
@@ -99,6 +148,8 @@ class Image_Segmentation(object):
         
         Search_Region_Coordinates = np.int0(np.zeros((1,4)))
         
+        cv2.imshow('ss',blurred)
+        cv2.waitKey()
         
         for i in range(len(c)):
             
@@ -135,121 +186,85 @@ class Image_Segmentation(object):
             cv2.imshow("draw_img", self.img_initial)
             cv2.waitKey()            
             
-        return Search_Region_Coordinates
+        return Search_Region_Coordinates, thresh_cover
 
 ###############################################################################
         
+        
 ###############################################################################
 #
-# Image Segmentation -> Search Regions (22.06.2018)
+# OCR matching -> Coordinates Location (28.06.2018)
 #
-###############################################################################    
+###############################################################################     
+
+class Operation_Location(object):
     
-class Window_Capture(object):
-    
-    def __init__(self,title):
+    def __init__(self, Search_Region,Image,hwnd,Target_Keyword):
         
-        self.title = title
+        self.SR = Search_Region
+        self.Image = Image
+        self.hwnd = hwnd
+        self.Target_Keyword = Target_Keyword
         
-    def Handle_Catch(self,hwnd,lParam):
+    def Get_Location(self) :
         
-        if win32gui.IsWindowVisible(hwnd):
+        left , top , _ , _ = win32gui.GetWindowRect(self.hwnd)
+        
+        for i in range(len(self.SR)):
             
-            if self.title in win32gui.GetWindowText(hwnd):
+            x1 = self.SR[i][0]
+            y1 = self.SR[i][1]
+            x2 = self.SR[i][2]
+            y2 = self.SR[i][3]
                 
-                self.handle = hwnd
+            hight = y2-y1
+            width = x2-x1
                 
-            else:
                 
-                self.handle = None
-   
-    def Image_Capture(self):
-        
-        win32gui.EnumWindows(a.Handle_Catch, None)
-        
-        if self.handle == None:
+            crop_img = self.Image[y1:y1+hight, x1:x1+width]
             
-            print('No Window Found!')
+            crop_img1 = cv2.bitwise_not(crop_img[int(0.05*hight):int(0.95*hight),int(0.06*width):int(0.94*width)])
             
-        else:
-            
-            win32gui.SetForegroundWindow(self.handle)
-            bbox = win32gui.GetWindowRect(self.handle)
-            img = ImageGrab.grab(bbox)
-            img.show()
-    
-        
+            #cv2.imshow('',crop_img1)
+            #cv2.waitKey()
                 
+            OCR_string = pytesseract.image_to_string(crop_img)
+            
+            if len(OCR_string) == 0:
+                
+                OCR_string = pytesseract.image_to_string(crop_img1)
+                
+                cv2.imwrite('icon'+str(i)+'.png', crop_img)
+                
+            print(OCR_string)
+                
+            if Levenshtein.ratio(OCR_string, self.Target_Keyword)>0.8:
+                pyautogui.moveTo(left+x1+abs(x2-x1)/2, top+y1+abs(y2-y1)/2)
+                pyautogui.click()  
+                break        
+            
+###############################################################################     
 
-a = Window_Capture('sasdfasdfs')
+lable = ['Activate Meshing 2222222222222222222','Close']
 
-        
-        
-        
+wintext = ['NX 1847','Physical']
 
 
 
-###############################################################################        
-
-a = Image_Segmentation('Image\\Search.png')
-
-b=a.Get_Region()
-
-label = 'NX 1847.1400 / Analysis_nx13.226' 
-
-hld = win32gui.FindWindow(None, label)
-
-#Tab_handle = win32gui.FindWindowEx(win32gui.FindWindow('#32770','???????V7.35'),None,'SysTabControl32','Tab1')
-
-left, top, right, bottom = win32gui.GetWindowRect(hld)
-
-Target_Keyword = 'Open'
-
-for i in range(len(b)):
+for i in range(len(lable)) :
     
-    if b[i][0]>=544 or b[i][1]>=163:
-        
-        continue
+    window = Window_Capture(wintext[i])
     
-    else:
+    img_initial,hwnd = window.Image_Capture()
     
-        x1 = b[i][0]
-        y1 = b[i][1]
-        x2 = b[i][2]
-        y2 = b[i][3]
-        
-        hight = y2-y1
-        width = x2-x1
-        
-        
-        crop_img= thresh_cover[y1:y1+hight, x1:x1+width]
-        
-        OCR_string = pytesseract.image_to_string(crop_img)
-        
-        print(OCR_string)
-        
-        if Levenshtein.ratio(OCR_string, Target_Keyword)>0.8:
-            #print(OCR_string)
-            #print(i)
-            #cv2.imwrite('icon\\icon'+str(i)+'.png',thresh_cover[y1:y1+hight, x1:x1+width])
-            pyautogui.moveTo(left+x1+abs(x2-x1)/2, top+y1+abs(y2-y1)/2)
-            pyautogui.click()  
-            break
+    img_process = Image_Segmentation(img_initial)
     
+    regions, img_processed = img_process.Get_Region(plot_flag=True)
     
-
-tempWindowName=win32gui.GetWindowText (win32gui.GetForegroundWindow())
-import time
-while True:
-    if (tempWindowName==win32gui.GetWindowText (win32gui.GetForegroundWindow()))
-        pass
-    else
-        tempWindowName=win32gui.GetWindowText (win32gui.GetForegroundWindow())
-        #do what you want
-    time.sleep(0.1)
+    e = Operation_Location(regions,img_processed,hwnd,lable[i])
+    
+    e.Get_Location()
+    
+    time.sleep(1)
 
     
-        
-a = Image_Segmentation('Image\\Search.png')
-
-b=a.Get_Region()
--- test.py
+++ test.py
@@ -1,46 +1,187 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 27 17:24:49 2018
+###############################################################################
+#
+# Import Modules
+#
+###############################################################################
+from PIL import Image, ImageGrab 
+# pytesseract import module should be modified if 'Image has no attribute Image' appears.
 
-@author: xqk9qq
-"""
+import pytesseract
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import Levenshtein
+pytesseract.pytesseract.tesseract_cmd = 'E:\\Development\\Tesseract-OCR\\tesseract'
 
-import win32gui
+import pyautogui
+import win32gui # OCR combined with win32 get handler and position.
 
+###############################################################################
+
+###############################################################################
+#
+# Image Segmentation -> Search Regions (22.06.2018)
+#
+###############################################################################
+
+class Image_Segmentation(object):
+    
+    def __init__(self,Operation_Image):
+        
+        self.Operation_Image = Operation_Image
+        
+    def Get_Region(self, plot_flag = None, Morph_kernel = None, Binary_Threshold = None):
+        
+        if plot_flag is None:
+            
+            plot_flag = False
+            
+        if Morph_kernel is None:
+            
+            Morph_kernel = (5,5)  
+            
+        if Binary_Threshold is None:
+            
+            Binary_Threshold = 180               
+                   
+        #self.img_initial = cv2.imread(self.Operation_Image)
+        
+        self.img_initial = self.Operation_Image
+        
+        h, w, _ = self.img_initial.shape
+        
+        gray = cv2.cvtColor(self.img_initial, cv2.COLOR_BGR2GRAY)
+        
+        # Delete the Long Edge
+        
+        (_, thresh) = cv2.threshold(gray, Binary_Threshold, 255, cv2.THRESH_BINARY_INV)     
+        
+        edges = cv2.Canny(thresh,100,200)
+        
+        lines = cv2.HoughLinesP(edges,1,np.pi/180,10, minLineLength = 20, maxLineGap = 0)     
+        
+        if lines is not None:
+
+            for i in range(len(lines)):
+                line = lines[i]    
+                cv2.line(thresh,(line[0][0],line[0][1]), (line[0][2],line[0][3]),(0,0,0),5)
+            
+        # Segmentation
+        # -> Can be optimized further in order to find more accurate parameter of morphology.
+        # -> For iteration, stop at the maximum box numbers.
+            
+        thresh_copy = thresh.copy()
+        
+        kernel_morph = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, Morph_kernel)
+        
+        closed = cv2.morphologyEx(thresh_copy, cv2.MORPH_CLOSE, kernel_morph) 
+        
+        kernel_dilate = np.uint8(np.ones((6,7)))
+        kernel_dilate[3,:]=0
+        
+        closed = cv2.dilate(closed, kernel_dilate, 5)
+        
+        #blurred = cv2.GaussianBlur(closed, (9, 9),0) # Gaussian Blur reduce noise
+        blurred = closed
+              
+        (_, cnts, _) = cv2.findContours(blurred, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        c = sorted(cnts, key=cv2.contourArea, reverse=True)
+        
+        # compute the rotated bounding box of the largest contour
+        
+        thresh_cover = cv2.bitwise_not(thresh)    
+        
+        Search_Region_Coordinates = np.int0(np.zeros((1,4)))
+             
+        for i in range(len(c)):
+            
+            rect = cv2.minAreaRect(c[i])
+            
+            box = np.int0(cv2.boxPoints(rect))
+            
+            Xs = np.int0([i[0] for i in box])
+            Ys = np.int0([i[1] for i in box])
+            
+            x1 = np.int0((min(Xs) + abs(min(Xs)))/2)
+            x2 = np.int0(max(Xs) if max(Xs) <= w else w)
+            y1 = np.int0((min(Ys) + abs(min(Ys)))/2)
+            y2 = np.int0(max(Ys) if max(Ys) <= h else h)
+            
+            hight = np.int0(y2-y1)
+            width = np.int0(x2-x1)
+            
+            print(y1,x1)
+            
+            box = np.int0([[x1,y1],[x2,y1],[x2,y2],[x1,y2]])
+            
+            corner = thresh_cover[y1:y1+hight, x1:x1+width]
+            
+            if i == 0:
+                
+                Search_Region_Coordinates[i][:] = corner
+            
+            else:
+                
+                if abs(x1-x2) >= 2 and abs(y1-y2) >= 2 :
+                
+                    Search_Region_Coordinates = np.append(Search_Region_Coordinates, corner,axis = 0)
+                    
+                    draw_img = cv2.drawContours(self.img_initial, [box], -1, (0, 0, 255), 3)
+                    
+        
+        if plot_flag is True:            
+        #cv2.imshow("blurred",blurred)      
+            cv2.imshow("draw_img", self.img_initial)
+            cv2.waitKey()            
+            
+        return Search_Region_Coordinates
+
+###############################################################################
+        
+###############################################################################
+#
+# Image Segmentation -> Search Regions (22.06.2018)
+#
+###############################################################################    
+    
 class Window_Capture(object):
     
     def __init__(self,title):
         
         self.title = title
         
+        self.handle = None
+        
     def Handle_Catch(self,hwnd,lParam):
         
-        if win32gui.IsWindowVisible(hwnd):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
             
             if self.title in win32gui.GetWindowText(hwnd):
                 
-                self.handle = hwnd
-                
-            else:
-                
-                self.handle = None
+                self.handle = hwnd     
    
     def Image_Capture(self):
         
         win32gui.EnumWindows(self.Handle_Catch, None)
         
-        if self.handle == None:
+        if self.handle is None:
             
             print('No Window Found!')
             
         else:
             
-            win32gui.SetForegroundWindow(self.handle)
             bbox = win32gui.GetWindowRect(self.handle)
-            img = ImageGrab.grab(bbox)
-            img.show()
-    
+            self.img = np.array(ImageGrab.grab(bbox))
+        
+        return self.img, self.handle
         
                 
 
-a = Window_Capture('ss')
+a = Window_Capture('Pre/Post')
+
+b,hwnd = a.Image_Capture()
+
+c = Image_Segmentation(b)
+
+d = c.Get_Region(plot_flag=True)
